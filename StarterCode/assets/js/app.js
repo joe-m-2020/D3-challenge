35140d17317eacfe6d4e3ffeec68d98834c2cfe0
Mr Magpie <joe.marrone@outlook.com>
2021-01-25 22:37:35 -0600

    app complete

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -19,44 +19,45 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import data from an external CSV file
-d3.csv("data/data.csv").then(function(censusData) {
+d3.csv("assets/data/data.csv").then(function(censusData) {
   console.log(censusData);
   console.log([censusData]);
     
   // Add X axis
-  var xrange = d3.extent(censusData.map(function(d){return d.income}))
+  var xrange = d3.extent(censusData.map(function(d){return d.income}));
 
   var x = d3.scaleLinear()
   .domain(xrange)
   .nice()
   .range([ 0, width ]);
-chartGroup.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
+  chartGroup.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
 // Add Y axis
-var yrange = d3.extent(censusData.map(function(d){return d.obesity}))
-var y = d3.scaleLinear()
+var yrange = d3.extent(censusData.map(function(d){return d.obesity}));
+  var y = d3.scaleLinear()
   .domain(yrange)
   .nice()
   .range([ height, 0]);
-chartGroup.append("g")
+  chartGroup.append("g")
   .call(d3.axisLeft(y));
 
-  var dots =chartGroup.append('g')
-    .selectAll()
-  .data(censusData)
-  .enter()
-  .append('g')
-  dots.append("circle")
-    .attr("cx", function (d) { return x(d.income); } )
-    .attr("cy", function (d) {  return y(d.obesity); } )
-    .attr("r", 10)
-    .style("fill", "#69b3a2")
+var dots =chartGroup.append('g')
+.selectAll()
+.data(censusData)
+.enter()
+.append('g')
+dots.append("circle")
+  .attr("cx", function (d) { return x(d.income); } )
+  .attr("cy", function (d) {  return y(d.obesity); } )
+  .attr("r", 10)
+  .style("fill", "#69b3a2");
 
 dots.append('text')
 .text(function(d) {return d.abbr})
-.attr("x", function (d) { return x(d.income)-10; } )
-.attr("y", function (d) {  return y(d.obesity)+8; } )
+  .attr("x", function (d) { return x(d.income)-8; } )
+  .attr("y", function (d) {  return y(d.obesity)+6; } )
+  .style("font-size", "13px")
 
 });
